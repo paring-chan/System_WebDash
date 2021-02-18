9598872d82ee902b21752b7a70347daa6c5a2399
pikokr <parangee9706@gmail.com>
2021-02-18 10:03:08 +0900

    feat: create user table query

---
+++ migrate.sql
@@ -0,0 +1,1 @@
+create table users (id text not null, token text);
--- utils/knex.ts
+++ utils/knex.ts
@@ -6,7 +6,8 @@ const knex = new Knex({
         host: process.env.DATABASE_HOST,
         port: Number(process.env.DATABASE_PORT),
         user: process.env.DATABASE_USER,
-        password: process.env.DATABASE_PASSWORD
+        password: process.env.DATABASE_PASSWORD,
+        database: process.env.DATABASE
     }
 })
 
