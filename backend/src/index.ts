4ae6f19068c0ee3039d07d53bdfaa16cfcebb153
pikokr <parangee9706@gmail.com>
2021-02-19 11:30:03 +0900

    feat: logger

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -3,9 +3,12 @@ import Router from "koa-router";
 import routes from './routes'
 import json from "koa-json";
 import {buildError} from "./utils/response";
+import logger from 'koa-logger'
 
 const app = new Koa()
 const router = new Router()
+
+app.use(logger())
 
 app.use(async (ctx, next) => {
     try {
@@ -13,24 +16,23 @@ app.use(async (ctx, next) => {
         const status = ctx.status || 404
         if (status === 404) {
             ctx.throw(404)
+            router.use('/', routes.routes())
         }
     } catch (err) {
         ctx.status = ctx.status || 500
         if (ctx.status === 404) {
+
+
+            app.use(json({
+                pretty: false
+            }))
+
             ctx.body = buildError({code: 404, message: 'Not Found'})
         } else {
             ctx.body = buildError({code: ctx.status, message: err.message})
         }
     }
 })
-
-app.use(json({
-    pretty: false
-}))
-
-
-router.use('/', routes.routes())
-
 app.use(router.routes())
 app.use(router.allowedMethods())
 
